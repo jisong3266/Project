78fb0b0f0ce52ba00d45f4b85e96d67c98e29fc1
jisong3266 <dbwlthd0726@naver.com>
2023-09-15 15:11:28 +0900

    1,2번

---
+++ p1.cpp
@@ -0,0 +1,53 @@
+#include <algorithm>
+#include <cctype>
+#include <cstdlib>
+#include <cstring>
+#include <ctime>
+#include <fstream>
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <vector>
+
+using namespace std;
+int i;
+
+int num(int n) {
+  for (int k = 0; k < n; k++) {
+    cout << i << endl;
+    if (i == 31) {
+      break;
+    }
+    i++;
+  }
+  return i;
+}
+int main() {
+  srand(time(NULL));
+  int user_num, pc_num;
+  i = 1;
+  cout << "~~~베스킨라빈스 31~~~\n";
+  while (i != 31) {
+    while (1) {
+      cout << "개수를 입력하세요: ";
+      cin >> user_num;
+      if (user_num < 1 || user_num > 3) {
+        cout << "1~3 사이의 수를 입력하세요\n";
+      } else {
+        break;
+      }
+    }
+    cout << "사용자가 부른 숫자!!" << endl;
+    num(user_num);
+    if (i == 31) {
+      cout << "게임 종료! 컴퓨터의 승리입니다.";
+      break;
+    }
+    cout << "컴퓨터가 부른 숫자!!" << endl;
+    pc_num = rand() % 3 + 1;
+    num(pc_num);
+    if (i == 31) {
+      cout << "게임 종료! 사용자의 승리입니다.";
+    }
+  }
+}
---
+++ p2.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <cctype>
+#include <cstring>
+#include <sstream>
+#include <algorithm>
+#include <fstream>
+#include <cstdlib>
+#include <ctime>
+
+using namespace std;
+
+int main(){
+  string word;
+  vector<string> word_box;
+  char first, last;
+  cout << "~~~끝말잇기(제한시간 30초)~~~\n";
+  cout << "첫 단어를 입력하세요 : ";
+  clock_t startTime = clock();
+  cin >> word;
+  last = word[word.size()-1];
+  word_box.push_back(word);
+  cout << word << endl;
+  double sum = 0;
+  while(1){
+    clock_t endTime = clock();
+    sum = double(endTime - startTime) / 100;
+    if( sum > 30){
+      cout << "타임 오버 !!\n 게임 종료!!";
+      cout << "총 입력한 단어 개수 : " << word_box.size()-1;
+      break;
+    }
+    cout << "다음 단어를 입력하세요 : ";
+    cin >> word;
+    first = word[0];
+    if(find(word_box.begin(),word_box.end(),word) != word_box.end()){
+      cout << "중복된 단어입니다.\n";
+      continue;
+  }
+    else if(last != first){
+      cout << "잘못된 입력입니다.\n";
+      continue;
+    }
+    last = word[word.size()-1];
+    word_box.push_back(word);
+    if(word_box.size() < 3){
+      cout << word_box[0] << "->" << word_box[1] << endl;;
+      }
+    else{
+      for(int i =0; i<word_box.size()-1;i++){
+      cout << word_box[i] << "->";
+      }
+      cout << word_box[word_box.size()-1] << endl;
+  }
+    }
+}
