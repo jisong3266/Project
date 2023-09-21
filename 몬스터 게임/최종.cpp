3a474506e3e9828258ce4a2a0a3530c78543ebf5
jisong3266 <dbwlthd0726@naver.com>
2023-09-22 06:23:30 +0900

    최종

---
+++ 몬스터 게임/최종.cpp
@@ -0,0 +1,362 @@
+#include "Character.h"
+#include "Monster.h"
+#include "Weapon.h"
+#include <algorithm>
+#include <cctype>
+#include <cstring>
+#include <ctime>
+#include <fstream>
+#include <iomanip>
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <vector>
+using namespace std;
+
+int ChooseCharacter(Character *character_bag[], int &count) {
+  string name;
+  cout << "캐릭터의 이름을 입력하세요 : ";
+  cin >> name;
+  Character *ch1 = new Character(name);
+  character_bag[count] = ch1;
+  count++;
+  cout << name << "캐릭터를 만들었습니다!" << endl;
+  return count - 1;
+}
+
+void ShowCharacterList(Character *character_bag[], int count) {
+  cout << "-----캐릭터 목록-----" << endl;
+  for (int i = 0; i < count; i++) {
+    cout << i << " : ";
+    character_bag[i]->Show_info();
+  }
+}
+
+int ChooseWeapon(Character *character_bag[], int character_num, int round,
+                 int &game_count, Weapon &w, Gun &g, Sword &s, Monster *m1) {
+  int weapon_number;
+  if (character_bag[character_num]->wpn[0] == "N/A" ||
+      (g.get() == 0 && s.get() == 0)) {
+    cout << "사용 가능한 무기가 없습니다. 무기를 주워주세요" << endl;
+    return -1;
+  } else {
+    for (int i = 0; i < character_bag[character_num]->wpn.size(); i++) {
+      cout << i << " : " << character_bag[character_num]->wpn[i]
+           << " 사용 가능 횟수 : ";
+      if (character_bag[character_num]->wpn[i] == "총")
+        cout << g.get() << endl;
+      else if (character_bag[character_num]->wpn[i] == "검")
+        cout << s.get() << endl;
+    }
+    cout << "사용할 무기 번호를 입력하세요 : ";
+    cin >> weapon_number;
+    if (character_bag[character_num]->wpn[weapon_number] == "총") {
+      if (g.get() == 0) {
+        cout << "사용 가능한 총이 없습니다. 다른 무기 번호를 입력하세요"
+             << endl;
+        return -1;
+      } else {
+        g.attack();
+        m1->hp -= g.power;
+        if (m1->hp <= 0) {
+          cout << "몬스터가 죽었습니다!\033[38;5;15m" << endl;
+          w.Level_up();
+          character_bag[character_num]->Level_up();
+          game_count = 0;
+          round++;
+          return -2;
+        } else {
+          cout << "몬스터의 남은 HP : " << m1->hp << endl;
+          return 0;
+        }
+      }
+    } else {
+      if (s.get() == 0) {
+        cout << "사용 가능한 검이 없습니다. 다른 무기 번호를 입력하세요"
+             << endl;
+        return -1;
+      } else {
+        s.attack();
+        m1->hp -= s.power;
+        if (m1->hp <= 0) {
+          cout << "몬스터가 죽었습니다!\033[38;5;15m" << endl;
+          w.Level_up();
+          game_count = 0;
+          round++;
+          return -2;
+        } else {
+          cout << "몬스터의 남은 HP : " << m1->hp << endl;
+          return 0;
+        }
+      }
+    }
+  }
+}
+
+void GainWeapon(Character *character_bag[], int character_num, Gun &g,
+                Sword &s) {
+  int random_number = rand() % 2 + 1;
+  if (random_number == 1) {
+    if (character_bag[character_num]->wpn[0] == "검") {
+      if (character_bag[character_num]->wpn.size() < 2) {
+        character_bag[character_num]->wpn.push_back("총");
+      }
+    } else if (character_bag[character_num]->wpn[0] != "총") {
+      character_bag[character_num]->wpn[0] = "총";
+    }
+    cout << "총을 주웠습니다!!" << endl;
+    g.gain();
+  } else if (random_number == 2) {
+    if (character_bag[character_num]->wpn[0] == "총") {
+      if (character_bag[character_num]->wpn.size() < 2) {
+        character_bag[character_num]->wpn.push_back("검");
+      }
+    } else if (character_bag[character_num]->wpn[0] != "검") {
+      character_bag[character_num]->wpn[0] = "검";
+    }
+    cout << "검을 주웠습니다!!" << endl;
+    s.gain();
+  }
+}
+
+int main() {
+  srand(time(NULL));
+  int num, count = 0, flag = 0, character_num, character_num2[2],
+           weapon_number = 0, random_number, box, round = 1, game_type,
+           game_level = 1, game_count;
+
+  string name;
+  Character ch;
+  Gun g;
+  Sword s;
+  Weapon w;
+  Character *character_bag[100] = {0};
+
+  cout << "~~~게임시작~~~" << endl;
+  cout << "게임 유형을 선택하세요 : (0: 1P, 1: 2P)";
+  cin >> game_type;
+
+  switch (game_type) {
+  case 0: {
+    game_level = 1;
+    cout << "1P를 선택했습니다" << endl;
+    if (count == 0) {
+      character_num = ChooseCharacter(character_bag, count);
+    }
+    Monster *m1 = new Monster(game_level);
+    box = game_level;
+    cout << "GAME LEVEL : " << game_level << endl;
+    m1->Show_info();
+
+    while (1) {
+      game_count = 0;
+      if (box != game_level) {
+        m1 = new Monster(game_level);
+        box = game_level;
+        cout << "GAME LEVEL : " << game_level << endl;
+        m1->Show_info();
+      }
+      box = game_level;
+      cout << endl << "----------------------------------" << endl;
+      cout << "숫자를 입력하세요(0:캐릭터 고르기, 1:캐릭터 만들기, 2:공격하기, "
+              "3:무기줍기, 4:종료)"
+           << endl;
+      cin >> num;
+
+      switch (num) {
+      case 1: {
+        character_num = ChooseCharacter(character_bag, count);
+        break;
+      }
+      case 0: {
+        ShowCharacterList(character_bag, count);
+        cout << "캐릭터 번호를 입력하세요 : ";
+        cin >> character_num;
+        break;
+      }
+      case 2: {
+        int weapon_choice = ChooseWeapon(character_bag, character_num,
+                                         game_count, round, w, g, s, m1);
+        if (weapon_choice == -1) {
+          continue;
+        }
+        if (weapon_choice == -2) {
+          game_level++;
+          character_bag[character_num]->hp += 10;
+          g.power += 5;
+          s.power += 5;
+          continue;
+        }
+        game_count = 1;
+        break;
+      }
+      case 3: {
+        game_count = 1;
+        GainWeapon(character_bag, character_num, g, s);
+        break;
+      }
+      case 4: {
+        flag = 1;
+        break;
+      }
+      }
+      if (flag == 1)
+        break;
+      if (game_level == box && game_count == 1) {
+        m1->m_attack();
+        character_bag[character_num]->hp -= m1->attack_power;
+        if (character_bag[character_num]->hp < 0) {
+          cout << "GAME OVER!\033[38;5;15m" << endl;
+          character_bag[character_num]->hp = 20;
+          g.power = 10;
+          s.power = 5;
+        } else
+          cout << " 남은 HP : " << character_bag[character_num]->hp
+               << "\033[38;5;15m";
+      }
+    }
+    break;
+  }
+  case 1: {
+    count = 0, flag = 0, weapon_number = 0, round = 1, game_level = 1;
+    bool player1_alive = true;
+    bool player2_alive = true;
+    int current_player = 1;
+
+    cout << "2P를 선택했습니다" << endl;
+    if (count == 0) {
+      cout << "Player1의 이름을 입력하세요 : ";
+      cin >> name;
+      Character *ch1 = new Character(name, 1);
+      character_bag[count] = ch1;
+      character_num2[0] = count;
+      count++;
+      cout << name << "캐릭터를 만들었습니다! 랜덤 HP : "
+           << character_bag[character_num2[0]]->hp << endl;
+      cout << "Player2의 이름을 입력하세요 : ";
+      cin >> name;
+      Character *ch2 = new Character(name, 1);
+      character_bag[count] = ch2;
+      character_num2[1] = count;
+      count++;
+      cout << name << "캐릭터를 만들었습니다! 랜덤 HP : "
+           << character_bag[character_num2[1]]->hp << endl;
+    }
+    Monster *m1 = new Monster(game_level);
+    box = game_level;
+    cout << "GAME LEVEL : " << game_level << endl;
+    m1->Show_info();
+
+    while (1) {
+      game_count = 0;
+      if (box != game_level) {
+        player1_alive = true;
+        player2_alive = true;
+        m1 = new Monster(game_level);
+        box = game_level;
+        cout << "GAME LEVEL : " << game_level << endl;
+        m1->Show_info();
+        cout << "Player1의 HP : " << character_bag[character_num2[0]]->hp
+             << endl;
+        cout << "Player2의 HP : " << character_bag[character_num2[1]]->hp
+             << endl;
+      }
+      box = game_level;
+      if (current_player == 1 && player1_alive)
+        cout << "\033[38;5;219mPlayer1\033[38;5;15m" << endl;
+      else if (current_player == 2 && player2_alive)
+        cout << "\033[38;5;214mPlayer2\033[38;5;15m" << endl;
+
+      cout << endl << "----------------------------------" << endl;
+      cout << "숫자를 입력하세요(0:캐릭터 고르기, 1:캐릭터 만들기, "
+              "2:공격하기, 3:무기줍기, 4:종료)"
+           << endl;
+      cin >> num;
+
+      switch (num) {
+      case 1: {
+        character_num2[current_player - 1] =
+            ChooseCharacter(character_bag, count);
+        break;
+      }
+      case 0: {
+        for (int i = 0; i < 2; i++) {
+          ShowCharacterList(character_bag, count);
+          cout << "Player" << i + 1 << "의 캐릭터 번호를 입력하세요 : ";
+          cin >> character_num2[i];
+          break;
+        }
+        break;
+      }
+      case 2: {
+        int weapon_choice =
+            ChooseWeapon(character_bag, character_num2[current_player - 1],
+                         game_count, round, w, g, s, m1);
+        if (weapon_choice == -1) {
+          continue;
+        }
+        if (weapon_choice == -2) {
+          game_level++;
+          character_bag[character_num2[0]]->hp += 10;
+          g.power += 5;
+          s.power += 5;
+          character_bag[character_num2[1]]->hp += 10;
+          continue;
+        }
+        game_count = 1;
+        break;
+      }
+      case 3: {
+        game_count = 1;
+        GainWeapon(character_bag, character_num2[current_player - 1], g, s);
+        break;
+      }
+      case 4: {
+        flag = 1;
+        break;
+      }
+      }
+      if (flag == 1)
+        break;
+      if (box == game_level && game_count == 1) {
+        m1->m_attack();
+        if (player1_alive && player2_alive) {
+          character_bag[character_num2[current_player - 1]]->hp -=
+              m1->attack_power;
+          if (character_bag[character_num2[current_player - 1]]->hp <= 0) {
+            if (current_player == 1) {
+              cout << "Player1이 죽었습니다!" << endl;
+              character_bag[character_num2[0]]->hp = 0;
+              player1_alive = false;
+              current_player = 2;
+            } else {
+              cout << "Player2가 죽었습니다!" << endl;
+              player2_alive = false;
+              character_bag[character_num2[1]]->hp = 0;
+              current_player = 1;
+            }
+          }
+          cout << "Player1의 HP : " << character_bag[character_num2[0]]->hp
+               << endl;
+          cout << "Player2의 HP : " << character_bag[character_num2[1]]->hp
+               << endl;
+        } else {
+          character_bag[character_num2[current_player - 1]]->hp -=
+              m1->attack_power;
+          cout << "남은 HP : "
+               << character_bag[character_num2[current_player - 1]]->hp << endl;
+        }
+      }
+      if (player1_alive && player2_alive) {
+        current_player = 3 - current_player; // 플레이어 차례 변경
+      }
+    }
+  }
+  }
+
+  for (int i = 0; i < count; i++) {
+    delete character_bag[i];
+  }
+
+  return 0;
+}
